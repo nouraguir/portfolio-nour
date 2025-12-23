ead805c410459f1d263b44dfb49d9bf5a625e063
nouraguir <nouraguir12@gmail.com>
2025-12-23 16:36:00 +0100

    initial commit

--- scriptansible.js
+++ scriptansible.js
@@ -1,62 +1,55 @@
-const images = [
+function imageSlider(imgId, images, delay = 3000) {
+  const img = document.getElementById(imgId);
+  if (!img) return;
+
+  let index = 0;
+  let intervalId = null;
+
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.isIntersecting && !intervalId) {
+        intervalId = setInterval(() => {
+          img.classList.add("fade-out");
+
+          setTimeout(() => {
+            index = (index + 1) % images.length;
+            img.src = images[index];
+            img.classList.remove("fade-out");
+          }, 400);
+        }, delay);
+      }
+
+      if (!entry.isIntersecting && intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+      }
+    },
+    { threshold: 0.6 }
+  );
+
+  observer.observe(img);
+}
+
+/* === PROJECT SLIDERS === */
+
+imageSlider("ansibleProjectImg", [
   "./images/A11.png",
   "./images/A111.png",
   "./images/a5.png",
   "./images/A4.png"
-];
+]);
 
-let currentIndex = 0;
-const imageElement = document.getElementById("ansibleProjectImg");
-
-setInterval(() => {
-  imageElement.style.opacity = 0;
-
-  setTimeout(() => {
-    currentIndex = (currentIndex + 1) % images.length;
-    imageElement.src = images[currentIndex];
-    imageElement.style.opacity = 1;
-  }, 400);
-
-}, 3000); 
-
-const ecommerceImages = [
+imageSlider("ecommerceImg", [
   "./images/E1.png",
   "./images/E4.png",
   "./images/E5.png",
   "./images/E6.png"
-];
+]);
 
-let ecommerceIndex = 0;
-const ecommerceImg = document.getElementById("ecommerceImg");
-
-setInterval(() => {
-  ecommerceImg.style.opacity = 0;
-
-  setTimeout(() => {
-    ecommerceIndex = (ecommerceIndex + 1) % ecommerceImages.length;
-    ecommerceImg.src = ecommerceImages[ecommerceIndex];
-    ecommerceImg.style.opacity = 1;
-  }, 400);
-
-}, 3000); 
-const angularEcommerceImages = [
+imageSlider("ecommerceAngularImg", [
   "./images/ang8.png",
   "./images/ang9.png",
   "./images/ang10.png",
   "./images/ang11.png",
   "./images/ang12.png"
-];
-
-let angularIndex = 0;
-const angularImg = document.getElementById("ecommerceAngularImg");
-
-setInterval(() => {
-  angularImg.style.opacity = 0;
-
-  setTimeout(() => {
-    angularIndex = (angularIndex + 1) % angularEcommerceImages.length;
-    angularImg.src = angularEcommerceImages[angularIndex];
-    angularImg.style.opacity = 1;
-  }, 400);
-
-}, 3000);
+]);
